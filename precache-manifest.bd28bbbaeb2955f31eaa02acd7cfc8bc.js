2431e76357d7b29ef2c7ee8047aff823e681dc1e
Вася Иванов <soldatik2005@mail.ru>
2020-06-05 16:41:54 +0300

    Updates

---
+++ precache-manifest.bd28bbbaeb2955f31eaa02acd7cfc8bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec8cf5b2fcc34e4585c68ddace6fdf1",
+    "url": "/test-bitnic/index.html"
+  },
+  {
+    "revision": "ad2a8cbc72c0f01b62be",
+    "url": "/test-bitnic/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "01ca5992ef4cae123417",
+    "url": "/test-bitnic/static/js/2.6f15c863.chunk.js"
+  },
+  {
+    "revision": "ad2a8cbc72c0f01b62be",
+    "url": "/test-bitnic/static/js/main.e16bb4ae.chunk.js"
+  },
+  {
+    "revision": "029380a36fcfa52f2865",
+    "url": "/test-bitnic/static/js/runtime-main.cb81d12c.js"
+  }
+]);
